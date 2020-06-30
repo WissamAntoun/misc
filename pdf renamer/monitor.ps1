11174200ed1b4ada8a33919a66f8c3565d9a27a0
wissam antoun <wissam.antoun@gmail.com>
2020-07-01 01:54:13 +0300

    added powershell script to monitor folder

---
+++ pdf renamer/monitor.ps1
@@ -0,0 +1,17 @@
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = "C:\Users\WISSAM-PC\Downloads\Documents\"
+$watcher.Filter = "*.*"
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+$action = { $path = $Event.SourceEventArgs.FullPath
+            $changeType = $Event.SourceEventArgs.ChangeType
+            $logline = "$(Get-Date), $changeType, $path"
+            Add-content "D:\pdf_log.txt" -value $logline
+            C:\Users\WISSAM-PC\.conda\envs\thesis\python.exe C:\repos\misc\rename_pdf_files_from_content_title_or_arxiv.py
+          }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED 
+Register-ObjectEvent $watcher "Created" -Action $action
+while ($true) {sleep 5}
